62921bf889ffe998177380e0ff5598776fbe35fa
shicks <shicks255@yahoo.com>
2017-10-28 14:52:11 -0400

    added a filter

--- js/controllers.js
+++ js/controllers.js
@@ -1,24 +1,29 @@
 angular.module('myApp.controllers', [])
 .controller('controller', function($scope, lastFmAPIservice)
 {
-    $scope.balls = 'balls';
-    $scope.artists = ["billy"];
+    $scope.artists = [];
+    $scope.searchFilter = function(song)
+    {
+        var keyword = new RegExp($scope.textFilter, 'i');
+        console.log(keyword.test(song.name));
+        return !$scope.textFilter || keyword.test(song.name) || keyword.test(song.artist.name) || keyword.test(song.playcount);
+    }
 
     lastFmAPIservice.getTracks().then(function(response)
     {
-        console.log(response.data.toptracks.track);
+        // console.log(response.data.toptracks.track);
         $scope.topTracks = response.data.toptracks.track;
     });
 
     lastFmAPIservice.getArtists().then(function(response)
     {
-        console.log(response.data);
+        // console.log(response.data);
         $scope.topArtists = response.data;
     });
 
     lastFmAPIservice.getAlbums().then(function(response)
     {
-        console.log(response.data);
+        // console.log(response.data);
         $scope.topAlbums = response.data;
     });
 
