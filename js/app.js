c26a3dadaf460a816df5e0984674bd936728c942
shicks <shicks255@yahoo.com>
2017-10-28 20:08:28 -0400

    added stuff for ngRoute

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
 angular.module('myApp', [
     'myApp.controllers',
-    'myApp.services'
+    'myApp.services',
+    'ngRoute'
 ])
-.config(function($sceDelegateProvider)
-{
-    $sceDelegateProvider.resourceUrlWhitelist([
-        'self',
-        'https://ws.audioscrobbler.com/**'
-    ]);
-});
+.config(function($routeProvider, $sceDelegateProvider)
+    {
+        $routeProvider
+            .when()
+            .when()
+            .otherwise();
+
+        $sceDelegateProvider.resourceUrlWhitelist([
+            'self',
+            'https://ws.audioscrobbler.com/**'
+        ]);
+    }
+);
+
+// angular.module('myApp', [
+//     'myApp.controllers',
+//     'myApp.services',
+//     'ngRoute'
+// ])
+// .config(function($sceDelegateProvider)
+// {
+//     $sceDelegateProvider.resourceUrlWhitelist([
+//         'self',
+//         'https://ws.audioscrobbler.com/**'
+//     ]);
+// });
